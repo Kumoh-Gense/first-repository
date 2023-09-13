a16ccdd993ef714b62edcf350ab540d10bb2a7ce
Kumoh-Gense <pinokio122622@gmail.com>
2023-09-13 15:15:11 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,10 @@
 def print_hello():
     print("Hello World!!")
 
+def Saying_File_Name():
+    print("This file is 'hello_world.py'")
+
 if __name__ == '__main__':
     print_hello()
+    Saying_File_Name()
     
